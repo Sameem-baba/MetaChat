a7e1b30ac204f7fc0144575eb24e4baf2691e9b4
Sameem-baba <ssssameembaba@gmail.com>
2021-12-16 18:04:53 +0530

    provider email added

--- components/Login.js
+++ components/Login.js
@@ -4,16 +4,19 @@ import { motion } from "framer-motion"
 import Particles from "react-tsparticles";
 import Config from "../assets/particle.json"
 import Loader from "./Loader";
+import { useRef } from "react";
 
 function Login() {
-    const { authenticate } = useMoralis();
+    const { authenticate, login, signup } = useMoralis();
+    const emailRef = useRef();
+    const passwordRef = useRef();
 
     return (
         <div
             className="bg-[#021b39] transition-all duration-700 ease-in-out relative"
         >  
             <Particles params={Config} className="w-full h-screen z-0" />
-                <div className="absolute items-center w-full flex flex-col top-48 space-y-4">
+            <div className="absolute items-center w-full flex flex-col top-28 space-y-4">
                 <motion.div className="" initial="hidden" animate="visible" variants={{
                     hidden: {
                         scale: .8,
@@ -35,7 +38,42 @@ function Login() {
                     />
                 </motion.div>
                 
-                
+                <div className='flex flex-col items-center '>
+                    <div className='bg-gray-100 rounded-lg w-64 p-2 flex items-center mb-3'>
+                        <input
+                            ref={emailRef}
+                            type="email"
+                            placeholder='Email'
+                            className='bg-gray-100 focus:outline-none flex-1'
+                        />
+                    </div>
+
+                    <div className='bg-gray-100 rounded-lg w-64 p-2 flex items-center'>
+                        <input
+                            ref={passwordRef}
+                            type="password"
+                            name='password'
+                            placeholder='Password' className='bg-gray-100 focus:outline-none flex-1'
+                        />
+                    </div>
+                            
+                    <div className='mt-3 flex flex-col space-y-4'>
+                        <button
+                            className='border-2 border-green-500 rounded-full px-12 py-2 inline-block text-white font-semibold hover:bg-yellow-500 hover:text-white'
+                            type='submit'
+                            onClick={() => login(emailRef, passwordRef)}
+                        >
+                            Sign In
+                        </button>
+                        <button
+                            className='border-2 border-green-500 rounded-full px-12 py-2 inline-block text-white font-semibold hover:bg-yellow-500 hover:text-white'
+                            type='submit'
+                            onClick={() => signup(emailRef.current.value, passwordRef.current.value, emailRef.current.value)}
+                        >
+                            Sign Up Now
+                        </button>
+                    </div>
+                </div>
                 <button
                     className="bg-yellow-500 rounded-lg p-5 font-bold animate-pulse "
                     onClick={authenticate}
--- pages/index.js
+++ pages/index.js
@@ -23,7 +23,7 @@ export default function Home() {
       </Head>
 
       <main className="flex flex-col items-center justify-center space-y-4 bg-black h-screen">
-        <h1 className='text-white '>Welcome to the Metaverse, {user.get("ethAddress")}</h1>
+        <h1 className='text-white '>Welcome to the Metaverse, {user.get("username")}</h1>
         <buttton className="text-white bg-yellow-500 p-5 rounded-lg cursor-pointer" onClick={logout}>Logout</buttton>
       </main>
     </div>
