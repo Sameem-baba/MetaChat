8825c63e6f2a67ee496ffd350ecb935e7eb854a9
Sameem-baba <ssssameembaba@gmail.com>
2021-12-18 21:24:49 +0530

    shadows added

--- components/ChangeProfile.js
+++ components/ChangeProfile.js
@@ -10,10 +10,10 @@ function ChangeProfile() {
 
     return (
         <div className="flex flex-col place-items-end space-y-4 text-sm absolute top-5 right-5 pr-3">
-            <button disabled={isUserUpdating} onClick={() => setOpel(true)} className="bg-gradient-to-tr from-purple-600 to-cyan-500 shadow-xl px-2 py-4 rounded-full text-white font-semibold  hover:bg-purple-400">
+            <button disabled={isUserUpdating} onClick={() => setOpel(true)} className="bg-gradient-to-tr from-purple-600 to-cyan-500 shadow-2xl px-2 py-4 rounded-full text-white font-semibold  hover:bg-purple-400">
                 Change Your Username
             </button>
-            <button disabled={isUserUpdating} onClick={() => setOpen(true)} className="shadow-xl bg-gradient-to-tr from-cyan-500 to-purple-600 px-2 py-4 rounded-full text-white font-semibold  hover:bg-purple-400">
+            <button disabled={isUserUpdating} onClick={() => setOpen(true)} className="shadow-2xl bg-gradient-to-tr from-cyan-500 to-purple-600 px-2 py-4 rounded-full text-white font-semibold  hover:bg-purple-400">
                 Change your Profile Picture
             </button>
         </div>
